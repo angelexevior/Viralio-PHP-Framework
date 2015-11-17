9f78e869436363e866bfaf5064fe5cfe3180d030
avakan01 <avakan01@ucy.ac.cy>
2015-11-17 19:36:08 +0200

    update headscript

--- templates/pieces/commons/headscript.php
+++ templates/pieces/commons/headscript.php
@@ -8,10 +8,3 @@ defined('_SECURED') or die('Restricted access');?>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-
-        
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
