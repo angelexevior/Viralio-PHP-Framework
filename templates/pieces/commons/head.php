0ef446f92e2d12902ca81c19af4956edbfa4b396
avakan01 <avakan01@ucy.ac.cy>
2015-11-17 18:54:59 +0200

    add headcss

--- templates/pieces/commons/head.php
+++ templates/pieces/commons/head.php
@@ -1,15 +1,8 @@
 <?php defined( '_SECURED' ) or die( 'Restricted access' ); ?>
 <head>
-       <?php include('commons/headmeta.php'); ?>
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Arimo:400,700,400italic">
-	<link rel="stylesheet" href="assets/css/fonts/linecons/css/linecons.css">
-	<link rel="stylesheet" href="assets/css/fonts/fontawesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" href="assets/css/xenon-core.css">
-	<link rel="stylesheet" href="assets/css/xenon-forms.css">
-	<link rel="stylesheet" href="assets/css/xenon-components.css">
-	<link rel="stylesheet" href="assets/css/xenon-skins.css">
-	<link rel="stylesheet" href="assets/css/custom.css">
+       <?php include('commons/headmeta.php'); 
+       include('commons/headcss.php'); ?>
+	
 
 	<script src="assets/js/jquery-1.11.1.min.js"></script>
 
---
+++ templates/pieces/commons/headcss.php
@@ -0,0 +1,14 @@
+<?php defined( '_SECURED' ) or die( 'Restricted access' ); ?>
+
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Arimo:400,700,400italic">
+	<link rel="stylesheet" href="assets/css/fonts/linecons/css/linecons.css">
+	<link rel="stylesheet" href="assets/css/fonts/fontawesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/xenon-core.css">
+	<link rel="stylesheet" href="assets/css/xenon-forms.css">
+	<link rel="stylesheet" href="assets/css/xenon-components.css">
+	<link rel="stylesheet" href="assets/css/xenon-skins.css">
+	<link rel="stylesheet" href="assets/css/custom.css">
+
+
+
