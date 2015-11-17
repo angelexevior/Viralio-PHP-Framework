74b8b44f02644734c9718e896edf9db80caa0587
gmoull01 <gmoull01@cs.ucy.ac.cy>
2015-11-17 18:16:46 +0200

    Start
    Created folders for project. Index.php shows dashboard1.html(in
    template.php)

--- templates/template.php
+++ templates/template.php
@@ -1,21 +1,7 @@
 <?php
 
-/*
- * Platform core templating file
- */
+include('pieces/source.php');
 
-//You can close and re-open the php tag in order to add plain HTML, CSS, JS or any other plain text scripting to your output. example below
-?>
-<h1>Template core file</h1>
-Hi. I am the core template file<br/>
-Please break your template in as many pieces as it logically makes sense<br/>
-<b>The secret is: PROVISIONS PROVISIONS PROVISIONS</b><br/>
-<a href="dashboard-1.html">Here is a sample of what you are working with</a>
 
-<?php
-/*
- * Note how we re-opened the php tag to include more code
- * You can do this as many times as you see fit
- * Keep it tidy !!!
- */
+
 ?>
