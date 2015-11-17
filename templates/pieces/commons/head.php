5eeeaa8505fbcaaac5d6e7750a2191352ea30c37
avakan01 <avakan01@ucy.ac.cy>
2015-11-17 19:33:42 +0200

    update head

--- templates/pieces/commons/head.php
+++ templates/pieces/commons/head.php
@@ -1,16 +1,8 @@
 <?php defined( '_SECURED' ) or die( 'Restricted access' ); ?>
 <head>
        <?php include('templates/pieces/commons/headmeta.php'); 
-       include('templates/pieces/commons/headcss.php'); ?>
-
-	<script src="assets/js/jquery-1.11.1.min.js"></script>
-
-	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
+       include('templates/pieces/commons/headcss.php');
+       include('templates/pieces/commons/headscript.php');?>
 
 </head>
 
