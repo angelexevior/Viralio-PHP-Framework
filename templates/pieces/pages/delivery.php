f3d54c916ffba15509a4468690222d75f217af5b
ddemet02 <ddemet02@cs.ucy.ac.cy>
2015-11-28 14:55:26 +0200

    Delivery Interface
    Delivery details about delivery progress and who delivers the order to
    destination.

--- templates/pieces/pages/delivery.php
+++ templates/pieces/pages/delivery.php
@@ -1,94 +1,92 @@
-<?php
-
-?>
+<?php ?>
 <h1>Delivery Details</h1>
 
 
 
 <div class="col-sm-15">
-								
-									<!-- Basic Table -->
-									
-									<table class="table responsive">
-										<thead>
-											<tr>
-												<th>#</th>
-												<th>Delivery Boy Name</th>
-												<th>Destination Address</th>
-                                                                                                <th>Progress</th>
-											</tr>
-										</thead>
-										
-										<tbody>
-											<tr>
-												<td>1</td>
-												<td>Giorgos Moullotos</td>
-												<td>Griva Digeni 3</td>
-                                                                                                <td>  <div class="progress">
-											<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
-												<span class="sr-only">70% Complete</span>
-											</div>
-                                                                                                    </div>
-                                                                                                    </td>
-										</tr>
-								<tr>
-												<td>2</td>
-												<td>Demetris Demetriades</td>
-												<td>Arxiepiskopou Makariou 1</td>
-                                                                                                <td>  <div class="progress">
-											<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-												<span class="sr-only">20% Complete</span>
-											</div>
-                                                                                                    </div>
-                                                                                                    </td>
-											</tr>
-											
-											<tr>
-												<td>3</td>
-												<td>Agathoklis Vakanas</td>
-												<td>Ledras 4</td>
-                                                                                <td><div class="progress">
-											<div class="progress-bar progress-bar-purple" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-												<span class="sr-only">40% Complete</span>
-											</div>
-										</div>
-                                                                                    </td>
-											</tr>
-                                                                                        <tr>
-												<td>4</td>
-												<td>Marios Papaiosif</td>
-												<td>Larnakos 29</td>
-                                                                                                <td><div class="progress">
-                                                                                                    <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 55%">
-												<span class="sr-only">55% Complete</span>
-											</div>
-                                                                                                </div> 
-                                                                                                    </td>
-											</tr>
-                                                                                        
-										</tbody>
-                                                                                
-									</table>
-									
-	
-                                                                      
-<button class="btn  btn-blue btn-icon btn-icon-standalone">
-									<i class="fa-print"></i>
-									<span>Print Receipt</span>
-								</button>
-<button class="btn btn-warning btn-icon">
-									<i class="fa-rocket"></i>
-									<span>Start Delivery</span>
-								</button>
-<button class="btn btn-success btn-icon ">
-									<i class="fa-check"></i>
-									<span>Done</span>
-								</button>
 
-<button class="btn btn-black btn-icon">
-									<span>Report problem </span>
-									<i class="fa-bullhorn"></i>
-								</button>                                                                        
+    <!-- Basic Table -->
+
+    <table class="table responsive">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Delivery Boy Name</th>
+                <th>Destination Address</th>
+                <th>Progress</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Giorgos Moullotos</td>
+                <td>Griva Digeni 3</td>
+                <td>  <div class="progress">
+                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+                            <span class="sr-only">70% Complete</span>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Demetris Demetriades</td>
+                <td>Arxiepiskopou Makariou 1</td>
+                <td>  <div class="progress">
+                        <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                            <span class="sr-only">20% Complete</span>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+
+            <tr>
+                <td>3</td>
+                <td>Agathoklis Vakanas</td>
+                <td>Ledras 4</td>
+                <td><div class="progress">
+                        <div class="progress-bar progress-bar-purple" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                            <span class="sr-only">40% Complete</span>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Marios Papaiosif</td>
+                <td>Larnakos 29</td>
+                <td><div class="progress">
+                        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 55%">
+                            <span class="sr-only">55% Complete</span>
+                        </div>
+                    </div> 
+                </td>
+            </tr>
+
+        </tbody>
+
+    </table>
+
+
+
+    <button class="btn  btn-blue btn-icon btn-icon-standalone">
+        <i class="fa-print"></i>
+        <span>Print Receipt</span>
+    </button>
+    <button class="btn btn-warning btn-icon">
+        <i class="fa-rocket"></i>
+        <span>Start Delivery</span>
+    </button>
+    <button class="btn btn-success btn-icon ">
+        <i class="fa-check"></i>
+        <span>Done</span>
+    </button>
+
+    <button class="btn btn-black btn-icon">
+        <span>Report problem </span>
+        <i class="fa-bullhorn"></i>
+    </button>                                                                        
 
 
 
