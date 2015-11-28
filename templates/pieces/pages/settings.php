976b63be651607d158d11fe0442fe26ae5a322fa
gerakl01 <gerakl01@cs.ucy.ac.cy>
2015-11-28 14:59:14 +0200

    settings update

--- templates/pieces/pages/settings.php
+++ templates/pieces/pages/settings.php
@@ -2,5 +2,35 @@
 
 ?>
 <h1>Settings</h1>
-This is the page for settings
+
+
+
+<br>
+<br>
+<br>
+
+<div   class="btn-group btn-group-justified">				
+								<a type="button" class="btn btn-primary">Users
+                                                              
+                                                                </a>
+                                                               
+								<a type="button" class="btn btn-blue">Products</a>
+							</div>  
+<br>
+
+<div class="btn-group btn-group-justified">			 		
+								<a type="button" class="btn btn-primary">Clients</a>
+								<a type="button" class="btn btn-blue">Orders</a>
+							</div>
+<br>
+
+<div class="btn-group btn-group-justified">			 		
+								<a type="button" class="btn btn-primary">Reports</a>
+								<a type="button" class="btn btn-blue">Routes</a>
+							</div>
+<br>
+<div class="btn-group btn-group-justified">			
+								<a type="button" class="btn btn-primary">Set up</a>
+								<a type="button" class="btn btn-blue">Suppliers</a>
+							</div>
 
