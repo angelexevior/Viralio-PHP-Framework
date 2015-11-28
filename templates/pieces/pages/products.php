8c8559a07741fa05dd3e902b05205bbc28c61f41
marios <mpapai04@ucy.ac.cy>
2015-11-28 15:02:23 +0200

    products and routes

--- templates/pieces/pages/products.php
+++ templates/pieces/pages/products.php
@@ -1,6 +1,130 @@
-<?php
-
-?>
+<?php ?>
 <h1>Products</h1>
 
-This is where the list of products will be shown
+<<div class="row">
+    <div class="col-md-12">
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="panel-options">
+                    <a href="#">
+                        <i class="linecons-cog"></i>
+                    </a>
+
+                    <a href="#" data-toggle="panel">
+                        <span class="collapse-icon">&ndash;</span>
+                        <span class="expand-icon">+</span>
+                    </a>
+
+                    <a href="#" data-toggle="reload">
+                        <i class="fa-rotate-right"></i>
+                    </a>
+
+                    <a href="#" data-toggle="remove">
+                        &times;
+                    </a>
+                </div>
+            </div>
+            <div class="panel-body">
+
+                <div class="table-responsive" data-pattern="priority-columns" data-focus-btn-icon="fa-asterisk" data-sticky-table-header="true" data-add-display-all-btn="true" data-add-focus-btn="true">
+
+                    <table cellspacing="0" class="table table-small-font table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th data-priority="1">Outlet_id</th>
+                                <th data-priority="3">product_id</th>
+                                <th data-priority="1">price</th>
+                                <th data-priority="3">discount</th>
+                                <th data-priority="3">quantity</th>
+                                <th data-priority="6">reorder_level</th>
+                                <th data-priority="1">delivery_type</th>
+                                <th data-priority="3">weight</th>
+                                <th data-priority="1">shipping_cost</th>
+                                <th data-priority="3">availability</th>
+                                <th data-priority="3">promo_price</th>
+                                <th data-priority="1">promo_end_date</th>
+                                <th data-priority="3">name</th>
+                                <th data-priority="1">alias</th>
+                                <th data-priority="3">description</th>
+                                <th data-priority="3">epos</th>
+                                <th data-priority="6">language</th>
+                                <th data-priority="1">updated</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1 <span class="co-name"></span></th>
+                                <td>1</td>
+                                <td>1.0</td>
+                                <td>5.00</td>
+                                <td>5%</td>
+                                <td>1000</td>
+                                <td>12</td>
+                                <td>2</td>
+                                <td>150gr</td>
+                                <td>1.70</td>
+                                <td>500</td>
+                                <td>1.0</td>
+                                <td>12/12/2016</td>
+                                <td>chicken hamburger</td>
+                                <td>chick</td>
+                                <td>zoumero</td>
+                                <td>tou 40</td>
+                                <td>en</td>
+                                <td>today</td>
+
+
+
+                            <tr>
+                                <th>2<span class="co-name"></span></th>
+                                <td>1</td>
+                                <td>1.0</td>
+                                <td>5.00</td>
+                                <td>5%</td>
+                                <td>1000</td>
+                                <td>12</td>
+                                <td>2</td>
+                                <td>150gr</td>
+                                <td>1.70</td>
+                                <td>500</td>
+                                <td>1.0</td>
+                                <td>12/12/2016</td>
+                                <td>pork hamburger</td>
+                                <td>por</td>
+                                <td>zoumero</td>
+                                <td>tou 40</td>
+                                <td>en</td>
+                                <td>today</td>
+                            </tr>		
+                            <tr>
+                                <th>3<span class="co-name"></span></th>
+                                <td>1</td>
+                                <td>1.0</td>
+                                <td>5.00</td>
+                                <td>5%</td>
+                                <td>1000</td>
+                                <td>12</td>
+                                <td>2</td>
+                                <td>150gr</td>
+                                <td>1.70</td>
+                                <td>500</td>
+                                <td>1.0</td>
+                                <td>12/12/2016</td>
+                                <td>salad</td>
+                                <td>sal</td>
+                                <td>fresh</td>
+                                <td>tou 40</td>
+                                <td>en</td>
+                                <td>today</td>
+                            </tr>
+
+
+
+                        </tbody>
+                    </table>
+
+                </div>
+
--- templates/pieces/pages/routes.php
+++ templates/pieces/pages/routes.php
@@ -1,6 +1,22 @@
-<?php
+<?php ?>
 
-?>
 <h1>Routes</h1>
+<!DOCTYPE html>
+<<html>
+   <html>
+<head>
+<style>
+body {
+    background-color:#93B874;
+}
+</style>
+</head>
+    <form action="http://maps.google.com/maps" method="get" target="_blank">
+        <strong>Enter your location!!</strong>
+        <input type="text" name="saddr" />
+        <input type="hidden" name="daddr" value="TGI Friday's, Verginas, Strovolos, Nicosia, Cyprus" />
+        <input type="submit" value="Get directions" />
+    </form>
+</body>
 
-This is where maps will be shown
+
