1aba847f8d88d677c078a49ac8b6416a2deb658f
avakan01 <avakan01@ucy.ac.cy>
2015-11-28 15:01:04 +0200

    clients

--- templates/pieces/pages/clients.php
+++ templates/pieces/pages/clients.php
@@ -1,6 +1,71 @@
-<?php
-
-?>
+<?php ?>
 <h1>Clients</h1>
-This is where the clients will be shown
+<div class="panel-body">
+    <a href="#" class="btn btn-info btn-sm btn-icon icon-left">
+        Add
+    </a>
+    <div class="row"><div class="col-xs-6"><div class="dataTables_length" id="example-1_length"><label>Show <select name="example-1_length" aria-controls="example-1" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option><option value="-1">All</option></select> entries</label></div></div><div class="col-xs-6"><div id="example-1_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="example-1"></label></div></div></div>    <div class="panel-body">
 
+        <div id="example-2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer"><table class="table table-bordered table-striped dataTable no-footer" id="example-2" role="grid" aria-describedby="example-2_info">
+                <thead>
+                    <tr role="row"><th class="no-sorting sorting_asc" rowspan="1" colspan="1" aria-label="
+                                       " style="width: 20px;">
+                            <div class="cbr-replaced"><div class="cbr-input"><input type="checkbox" class="cbr cbr-done"></div><div class="cbr-state"><span></span></div></div>
+                        </th><th class="sorting" tabindex="0" aria-controls="example-2" rowspan="1" colspan="1" aria-label="#: activate to sort column ascending" style="width: 10px;">#</th><th class="sorting" tabindex="0" aria-controls="example-2" rowspan="1" colspan="1" aria-label="Name: activate to sort column ascending" style="width: 150px;">Name </th><th class="sorting" tabindex="0" aria-controls="example-2" rowspan="1" colspan="1" aria-label="Address: activate to sort column ascending" style="width: 150px;">Address</th><th class="sorting" tabindex="0" aria-controls="example-2" rowspan="1" colspan="1" aria-label="Telephone: activate to sort column ascending" style="width: 75px;">Telephone</th><th class="sorting" tabindex="0" aria-controls="example-2" rowspan="1" colspan="1" aria-label="Actions: activate to sort column ascending" style="width: 10px;">Actions</th></tr>
+                </thead>
+
+                <tbody class="middle-align">
+
+                    <tr role="row" class="odd">
+                        <td class="sorting_1">
+                            <div class="cbr-replaced"><div class="cbr-input"><input type="checkbox" class="cbr cbr-done"></div><div class="cbr-state"><span></span></div></div>
+                        </td>
+                        <td>1</td>
+                        <td>Agathoklis Vakanas</td>
+                        <td>Elepherias 48,Korakou</td>
+                        <td>99991199</td>
+                        <td>
+                            <a href="#" class="btn btn-secondary btn-sm btn-icon icon-left">
+                                Edit
+                            </a>
+                            <a href="#" class="btn btn-danger btn-sm btn-icon icon-left">
+                                Delete
+                            </a>                          
+                        </td>
+
+                    </tr><tr role="row" class="even">
+                        <td class="sorting_1">
+                            <div class="cbr-replaced"><div class="cbr-input"><input type="checkbox" class="cbr cbr-done"></div><div class="cbr-state"><span></span></div></div>
+                        </td>
+                        <td>2</td>
+                        <td>Andreas Kizis</td>
+                        <td>Apostolou Markou 2, Lakatamia</td>
+                        <td>22962583</td>
+                        <td>
+                            <a href="#" class="btn btn-secondary btn-sm btn-icon icon-left">
+                                Edit
+                            </a>
+                            <a href="#" class="btn btn-danger btn-sm btn-icon icon-left">
+                                Delete
+                            </a>
+                        </td>
+                    </tr><tr role="row" class="even">
+                        <td class="sorting_1">
+                            <div class="cbr-replaced"><div class="cbr-input"><input type="checkbox" class="cbr cbr-done"></div><div class="cbr-state"><span></span></div></div>
+                        </td>
+                        <td>3</td>
+                        <td>Demetris Demetriadis</td>
+                        <td>Griva Digeni 23, Strovolo</td>
+                        <td>96300456</td>
+                        <td>
+                            <a href="#" class="btn btn-secondary btn-sm btn-icon icon-left">
+                                Edit
+                            </a>
+                            <a href="#" class="btn btn-danger btn-sm btn-icon icon-left">
+                                Delete
+                            </a>
+                        </td>
+                    </tr></tbody>
+            </table><div class="row"><div class="col-xs-15"><div class="dataTables_info" id="example-2_info" role="status" aria-live="polite">Showing 1 to 3 of 3 entries</div></div><div class="col-xs-6"><div class="dataTables_paginate paging_simple_numbers" id="example-2_paginate"><ul class="pagination"><li class="paginate_button previous disabled" aria-controls="example-2" tabindex="0" id="example-2_previous"><a href="#">Previous</a></li><li class="paginate_button active" aria-controls="example-2" tabindex="0"><a href="#">1</a></li><li class="paginate_button " aria-controls="example-2" tabindex="0"><a href="#">2</a></li><li class="paginate_button " aria-controls="example-2" tabindex="0"><a href="#">3</a></li><li class="paginate_button " aria-controls="example-2" tabindex="0"><a href="#">4</a></li><li class="paginate_button " aria-controls="example-2" tabindex="0"><a href="#">5</a></li><li class="paginate_button " aria-controls="example-2" tabindex="0"><a href="#">6</a></li><li class="paginate_button next" aria-controls="example-2" tabindex="0" id="example-2_next"><a href="#">Next</a></li></ul></div></div></div></div>
+    </div>
+</div>
